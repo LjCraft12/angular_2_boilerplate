cdb05766f999b3246b922922d9ef5e096516c307
Perry Craft <ljcraft12@gmail.com>
2017-06-21 16:35:29 -0400

    user creation is done and hashes password before saving to the database

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,42 @@
 // Required modules
-const express = require('express');
-const router = express.Router();
+const express  = require('express');
+const router   = express.Router();
+const passport = require('passport');
+const jwt      = require('jsonwebtoken')
+
+
+// Import user model for the register route
+const User = require('../models/user');
 
 // Register route
-router.get('/register', (req, res, next) => {
-    res.send('Hello Register')
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        firstName: req.body.firstName,
+        lastName:  req.body.lastName,
+        email:     req.body.email,
+        username:  req.body.username,
+        password:  req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+       // Check for error on adding user
+        if (err) {
+            res.json({
+                success: false,
+                message: 'Failed to register user'
+            });
+            // If user is created
+        } else {
+            res.json({
+                success: true,
+                message: 'User registered successfully'
+            });
+        }
+    });
 });
 
 // Authenticate route
-router.get('/authenticate', (req, res, next) => {
+router.post('/authenticate', (req, res, next) => {
     res.send('Hello Auth')
 });
 
